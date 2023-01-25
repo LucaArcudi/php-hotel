c3a0494f9c697ef7416eaa278f43955908045264
LucaArcudi <arcudilu@gmail.com>
2023-01-25 21:46:24 +0100

    echo'ed arrays value

--- index.php
+++ index.php
@@ -10,6 +10,192 @@
 
 <body>
 
+    <?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+    ?>
+
+    <main>
+        <div class="hotel">
+            <p>
+                <?php
+                echo $hotels[0]["name"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[0]["description"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[0]["parking"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[0]["vote"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[0]["distance_to_center"];
+                ?>
+            </p>
+        </div>
+
+        <div class="hotel">
+            <p>
+                <?php
+                echo $hotels[1]["name"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[1]["description"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[1]["parking"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[1]["vote"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[1]["distance_to_center"];
+                ?>
+            </p>
+        </div>
+
+        <div class="hotel">
+            <p>
+                <?php
+                echo $hotels[2]["name"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[2]["description"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[2]["parking"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[2]["vote"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[2]["distance_to_center"];
+                ?>
+            </p>
+        </div>
+
+        <div class="hotel">
+            <p>
+                <?php
+                echo $hotels[3]["name"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[3]["description"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[3]["parking"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[3]["vote"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[3]["distance_to_center"];
+                ?>
+            </p>
+        </div>
+
+        <div class="hotel">
+            <p>
+                <?php
+                echo $hotels[4]["name"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[4]["description"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[4]["parking"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[4]["vote"];
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $hotels[4]["distance_to_center"];
+                ?>
+            </p>
+        </div>
+    </main>
+
 </body>
 
 </html>
